816dec28eeab5e39cca46f3ff9727896215d7f02
daoge_cmd <3523206925@qq.com>
2024-11-20 16:55:30 +0800

    docs: update README.md

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # RemoteCommand
 
-Execute server command through HTTP interface.
+Execute server command through http interface.
 
 This plugin is used in allay official test server to add support for auto update.
 
-# Configuration
-
-TODO
-
 # Http API
 
-TODO
+Let's say you set http server port to 19133, then the base url is `http://localhost:19133/remotecommand`.
+Please note that only POST method is allowed!
+
+Parameters:
+- `token` The token you set in `config.yml`.
+- `command` The command you want to execute.
 
 ## License
 
--- src/main/java/org/allaymc/remotecommand/Config.java
+++ src/main/java/org/allaymc/remotecommand/Config.java
@@ -1,6 +1,7 @@
 package org.allaymc.remotecommand;
 
 import eu.okaeri.configs.OkaeriConfig;
+import eu.okaeri.configs.annotation.Comment;
 import eu.okaeri.configs.annotation.CustomKey;
 import lombok.Getter;
 import lombok.experimental.Accessors;
@@ -12,7 +13,9 @@ import org.apache.commons.lang3.RandomStringUtils;
 @Getter
 @Accessors(fluent = true)
 public class Config extends OkaeriConfig {
+    @Comment("The token used to verify the request. Do not share it to other!")
     private String token = RandomStringUtils.randomAlphanumeric(32);
+    @Comment("The port of the http server")
     @CustomKey("http-server-port")
     private int httpServerPort = 19133;
 }
